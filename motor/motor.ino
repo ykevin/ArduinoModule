0d41169713231f8c75860ec8b2c87ac8eb83d30e
ykevin <15018787835@163.com>
2017-01-05 22:57:52 +0800

    add motor ccp lib

--- motor/motor.ino
+++ motor/motor.ino
@@ -1,59 +1,46 @@
-#define AIN1	9 
-#define AIN2	10 
-#define BIN1	11 
-#define BIN2	12 
-#define PWMA  	5
-#define PWMB  	6 
+#include "Motor.h"
+
+#define MOTOR1_IN_A		9 
+#define MOTOR1_IN_B		10 
+#define MOTOR2_IN_A		11 
+#define MOTOR2_IN_B		12 
+#define MOTOR1_PWM  	5
+#define MOTOR2_PWM  	6 
  
 #define STBY	4  
+
+//left side motors
+Motor motor1(MOTOR1_PWM, MOTOR1_IN_A, MOTOR1_IN_B); //front
+
+ //right side motors
+Motor motor2(MOTOR2_PWM, MOTOR2_IN_A, MOTOR2_IN_B); // front
+
   
 void setup() {  
-//  Serial.begin (9600);  
-//初始化各IO,模式为OUTPUT 输出模式  
-  pinMode(AIN1,OUTPUT);  
-  pinMode(AIN2,OUTPUT); 
-  pinMode(BIN1,OUTPUT);  
-  pinMode(BIN2,OUTPUT); 
-  pinMode(PWMA, OUTPUT); 
-  pinMode(PWMB, OUTPUT); 
+ 	
+        pinMode(STBY, OUTPUT); 
 
-  pinMode(STBY, OUTPUT); 
-
-  digitalWrite(STBY, HIGH);  
+  	digitalWrite(STBY, HIGH);  
 }  
   
 void loop() {  
-  //forward 向前转  
-  digitalWrite(AIN1,HIGH); //给高电平  
-  digitalWrite(AIN2,LOW);  //给低电平 
-  digitalWrite(BIN1,HIGH); //给高电平  
-  digitalWrite(BIN2,LOW);  //给低电平 
+ 	motor1.spin(100); 
+ 	motor2.spin(100); 
 
-  analogWrite(PWMA, 150); 
-  analogWrite(PWMB, 150);
-  delay(2000);   //延时2秒  
+	delay(2000);	
+  	digitalWrite(STBY, LOW);  
   
-  //stop 停止  
-  digitalWrite(STBY, LOW);  
-  delay(2000);
-  digitalWrite(STBY, HIGH);  
- 
-  
-    
-  //back 向后转  
-  digitalWrite(AIN1,LOW);  
-  digitalWrite(AIN2,HIGH);
-  digitalWrite(BIN1,LOW);   //给高电平  
-  digitalWrite(BIN2,HIGH);  //给低电平 
+	delay(1000);
+  	digitalWrite(STBY, HIGH);  
 
-  analogWrite(PWMA, 150); 
-  analogWrite(PWMB, 150); 
-  delay(2000);  
+ 	motor1.spin(-100); 
+ 	motor2.spin(-100); 
 
-  digitalWrite(STBY, LOW);  
-  delay(2000);
-  digitalWrite(STBY, HIGH);    
+	delay(2000);	
+  	digitalWrite(STBY, LOW);  
   
+	delay(2000);
+  	digitalWrite(STBY, HIGH);  
 }  
 
 
